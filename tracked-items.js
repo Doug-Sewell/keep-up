47af89b9669f6ff20137be00a2d097ecbca31b58
Doug Sewell <DouglasASewell@gmail.com>
2020-07-05 18:58:54 -0400

    Added ability to create new categories

--- tracked-items.js
+++ tracked-items.js
@@ -11,36 +11,36 @@ const listAllItems = () => {
 //Lists all todos in a user provided category.
 const listAllTodos = (category) => {
     const items = getItems();
-    
+
     //Check if the category exists. If no category is found, the else statement executes.
     const itemTodos = items.find(topic => {
         return topic.category == category;
     });
-    if(itemTodos) {
+    if (itemTodos) {
 
         console.log(chalk.blue.inverse('Your tracked tasks:'));
         itemTodos.items.forEach(item => {
             //Reference created to the amount of days since work was last done and the current date.
             const daysDifference = getDaysSinceWorked(item.date);
             const date = new Date(item.date);
-            
+
             //Check if todo is overdue or not. Green for good standing, yellow for about to be overdue, or red for overdue.
-            if(daysDifference < item.daysToWork) {
+            if (daysDifference < item.daysToWork) {
                 console.log(chalk.bold.green.inverse('Topic: ' + item.topic));
-            } else if(daysDifference == item.daysToWork) {
+            } else if (daysDifference == item.daysToWork) {
                 console.log(chalk.bold.yellow.inverse('Topic: ' + item.topic));
-            } else if(daysDifference > item.daysToWork) {
+            } else if (daysDifference > item.daysToWork) {
                 console.log(chalk.bold.red.inverse('Topic: ' + item.topic));
             }
             console.log('- Last Worked on ' + date.toDateString());
             console.log('- Days to work without being overdue: ' + item.daysToWork);
             console.log(`- Days since worked: ${daysDifference}`);
-            if(daysDifference < item.daysToWork) {
-                console.log ('- Status: Good Standing');
-            } else if(daysDifference == item.daysToWork) {
-                console.log ('- Status: Must progress today to remain in good standing');
-            } else if(daysDifference > item.daysToWork) {
-                console.log ('- Status: Overdue');
+            if (daysDifference < item.daysToWork) {
+                console.log('- Status: Good Standing');
+            } else if (daysDifference == item.daysToWork) {
+                console.log('- Status: Must progress today to remain in good standing');
+            } else if (daysDifference > item.daysToWork) {
+                console.log('- Status: Overdue');
             }
         });
     } else {
@@ -68,7 +68,7 @@ const addTodo = (todo, category, daysToWork) => {
     if (duplicateTodo.length === 0) {
         const doesCategoryExist = items.find(item => item.category === category);
         if (doesCategoryExist === undefined) {
-            console.log(chalk.red.inverse(`Error: The category of ${category} does not exist.`)); 
+            console.log(chalk.red.inverse(`Error: The category of ${category} does not exist.`));
             console.log(chalk.red.inverse(`Please add the category and try again or place the new tracked item in a category that already exists.`));
         } else {
             //Automatically creates a new todo with the current date.
@@ -99,13 +99,25 @@ const addTodo = (todo, category, daysToWork) => {
 //Add a category
 const addCategory = categoryInput => {
     const items = getItems();
+    let addCategory = true;
+
     items.forEach(category => {
-        if(category.category.toLowerCase() == categoryInput.toLowerCase()) {
-            console.log(chalk.red.inverse('Error: A category with this name already exists.'));
-        } else {
-            console.log(chalk.green.inverse('Hooray! This can be added.'));
+        if (category.category.toLowerCase() == categoryInput.toLowerCase()) {
+            addCategory = false;
         }
-    })
+    });
+
+    if (addCategory) {
+        console.log(chalk.green.inverse(`Success! Created ${categoryInput} as a category.`));
+        const newCategory = {
+            'category': categoryInput,
+            'items': []
+        }
+        items.push(newCategory);
+        saveTodos(items);
+    } else {
+        console.log(chalk.red.inverse('Error: A category with this name already exists. Please use another category name.'));
+    }
 }
 
 //Returns the saved data as JSON
@@ -126,8 +138,8 @@ const getDaysSinceWorked = (givenDate) => {
     const now = getToday();
     const date = new Date(givenDate);
     const dateMilliseconds = date.getTime();
-    const difference =  now - dateMilliseconds;
-    const days = Math.floor(difference/1000/60/60/24);
+    const difference = now - dateMilliseconds;
+    const days = Math.floor(difference / 1000 / 60 / 60 / 24);
     return days;
 }
 
